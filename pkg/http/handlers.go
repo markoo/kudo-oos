2256084f7c0938b41a94157556260231dfb3894f
Mark Steven Alan Thomas <mark@thomas.as>
2020-05-04 22:12:17 +0200

    first commit

---
+++ pkg/http/handlers.go
@@ -0,0 +1,98 @@
+package http
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/markoo/kudo-oos/pkg/core"
+	"github.com/markoo/kudo-oos/pkg/kudo"
+)
+
+// Service services
+type Service struct {
+	repo   core.Repository
+	Router http.Handler
+}
+
+// New does what it says
+func New(repo core.Repository) Service {
+	service := Service{
+		repo: repo,
+	}
+
+	router := httprouter.New()
+	router.GET("/kudos", service.Index)
+	router.POST("/kudos", service.Create)
+	router.DELETE("/kudos/:id", service.Delete)
+	router.PUT("/kudos/:id", service.Update)
+
+	service.Router = UseMiddlewares(router)
+
+	return service
+}
+
+// Index -ing all things
+func (s Service) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	kudos, err := service.GetKudos()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(kudos)
+}
+
+// Create it
+func (s Service) Create(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	payload, _ := ioutil.ReadAll(r.Body)
+
+	githubRepo := kudo.GitHubRepo{}
+	json.Unmarshal(payload, &githubRepo)
+
+	kudo, err := service.CreateKudoFor(githubRepo)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(kudo)
+}
+
+// Delete now
+func (s Service) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+
+	repoID, _ := strconv.Atoi(params.ByName("id"))
+	githubRepo := kudo.GitHubRepo{RepoID: int64(repoID)}
+
+	_, err := service.RemoveKudo(githubRepo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+// Update and be gone
+func (s Service) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	payload, _ := ioutil.ReadAll(r.Body)
+
+	githubRepo := kudo.GitHubRepo{}
+	json.Unmarshal(payload, &githubRepo)
+
+	kudo, err := service.UpdateKudoWith(githubRepo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(kudo)
+}
